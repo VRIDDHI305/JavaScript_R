703da6f44dd257721ee0714ca2f3949fc033736d
VRIDDHI305 <141445922+VRIDDHI305@users.noreply.github.com>
2024-11-21 14:47:42 +0000

    basics

---
+++ JavaScript/01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 9876543;
+let accountEmail = "viriddhi@gmail.com";
+var accountPassword = "123456";
+accountCity = "Raipur";
+
+//accountId = 23 // not allowed
+
+accountEmail = "jkdh@gmail.com";
+accountPassword = "6478749949";
+accountCity = "MP";
+
+console.log(accountId);
+console.table([accountEmail, accountId, accountCity, accountPassword]);
---
+++ JavaScript/01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict" //treat all JS code as newer version
+
+//alert(3+3)//we are using node js, not browser
+
+console.log(3+3)
+console.log("vriddhi")
+
+
+
+
+
+//number ==> 2 to power 53
+// bigint for big values
+// string ==>""
+// boolean ==>true or false
+//null => standalone value
+//undefined =>
+//symbol => unique (majorly used when react) 
+
+
+//object
+
+
+console.log(typeof "vriddhi");//typeof used to define what is the tyoe of the value
+
